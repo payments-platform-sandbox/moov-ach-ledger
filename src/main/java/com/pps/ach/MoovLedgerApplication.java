952adfa8cacc7e08a0d897da48520f72d53e6aef
Satnam Walia <42784435+satnam-walia@users.noreply.github.com>
2025-09-02 15:26:29 -0400

    init project structure + pom generation

---
+++ src/main/java/com/pps/ach/MoovLedgerApplication.java
@@ -0,0 +1,11 @@
+package com.pps.ach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoovLedgerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoovLedgerApplication.class, args);
+    }
+}
